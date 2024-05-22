a87d26ad2021043d5ae33484fcc87accd44fa4d3
prottoy5 <106944019+prottoy5@users.noreply.github.com>
2024-05-22 15:51:33 +0600

    Update rest_api.dart

--- lib/rest_api/rest_api.dart
+++ lib/rest_api/rest_api.dart
@@ -1,26 +1,29 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../model/component.dart';
 
 class GetPhotos {
   Future<List<Photos>?> getPhotos() async {
+    List<Photos> photos = <Photos>[];
     try {
       final http.Response response = await
       http.get(Uri.parse("https://jsonplaceholder.typicode.com/photos"));
       if (response.statusCode == 200) {
-        final List<dynamic> json = jsonDecode(response.body);
-        return photosFromJson(json);
+        final List<dynamic> jsonData = jsonDecode(response.body);
+        for(Map<String,dynamic> json in jsonData){
+          photos.add(Photos.fromJson(json));
+        }
+        return photos;
       } else {
         throw Exception('Failed to fetch photos');
       }
     } catch (e) {
-      debugPrint(e.toString);
+      if (kDebugMode) {
+        debugPrint(e.toString());
+      }
       return null;
     }
   }
 }
-
-
-
-
 
